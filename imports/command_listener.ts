586adeaab7a1cbfa34f5faeee905ae0e98aff20b
Carbonax <gizzib0129@gmail.com>
2022-04-21 09:33:23 +0100

    Initial Code

--- imports/command_listener.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "discord.js"
-
-export default (client: Client, name: string, callback: Function) => {
-    client.on('interactionCreate', (interaction) => {
-        if(!interaction.isCommand()) return
-        if(interaction.commandName === name.toLowerCase()) callback (interaction)
-    })
-}
